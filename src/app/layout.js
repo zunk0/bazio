aba221e4703411fb9072b6c8a3cea5bffb76368f
DaviddTheCoder <davidlexmann@s.zochova.sk>
2026-04-15 18:56:55 +0200

    initial nextjs setup with tailwind

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,8 +18,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
-      <body>{children}</body>
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
 }
